5eb5788836b40ae5e477744550416b0e666cfda9
nataliesun <natsun121@gmail.com>
2019-05-30 19:22:04 -0700

    Updates

---
+++ precache-manifest.e3d0834d87020498e36dbf7793587e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d91ba241fd096232dafd1decc1d7143",
+    "url": "/surf-buddy/index.html"
+  },
+  {
+    "revision": "3cb5c9088bee35fc9787",
+    "url": "/surf-buddy/static/css/main.b4b8ed13.chunk.css"
+  },
+  {
+    "revision": "625a1f97eee0596788a3",
+    "url": "/surf-buddy/static/js/2.69124a38.chunk.js"
+  },
+  {
+    "revision": "3cb5c9088bee35fc9787",
+    "url": "/surf-buddy/static/js/main.a42ff7a7.chunk.js"
+  },
+  {
+    "revision": "935724a1c01e34084ed9",
+    "url": "/surf-buddy/static/js/runtime~main.3272d286.js"
+  },
+  {
+    "revision": "7f8035af5f3f08e8ed24a3d90d494219",
+    "url": "/surf-buddy/static/media/welcome-hero.7f8035af.jpg"
+  }
+]);
